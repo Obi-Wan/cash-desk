87d40e9ebfd2dee8b9ffd6dacc005c87acb5b822
Nicola <ben.vighy@gmail.com>
2009-04-28 18:46:55 +0200

    Initial commit of empty project

---
+++ src/gestionecassa/Ordine.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestionecassa;
+
+/**
+ *
+ * @author ben
+ */
+public class Ordine {
+
+    int birre;
+    int panini;
+    int piadine;
+    int bibite;
+}
